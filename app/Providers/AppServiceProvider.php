bfb4d3f6d21ebbc9c6e7a58e3a07fc0ed3245271
Mihai Solomon <mihai.c.solomon@gmail.com>
2019-11-23 19:47:26 +0200

    Logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Repositories\Ideas\IdeasRepository;
 use App\Repositories\Ideas\IdeasRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use Spatie\HttpLogger\LogProfile;
+use Spatie\HttpLogger\LogWriter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,10 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/http-logger.php', 'http-logger');
     }
 
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/http-logger.php' => config_path('http-logger.php'),
+            ], 'config');
+        }
+
+        $this->app->singleton(LogProfile::class, config('http-logger.log_profile'));
+        $this->app->singleton(LogWriter::class, config('http-logger.log_writer'));
+
         $this->app->bind(IdeasRepositoryInterface::class, IdeasRepository::class);
     }
 }
